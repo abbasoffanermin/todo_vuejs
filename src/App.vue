1e68d90a17ebeedc6fe9a9cde00e82a6fc002c88
abbasoffanermin <105522485+abbasoffanermin@users.noreply.github.com>
2022-11-23 12:25:35 +0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+
+<script setup>
+
+import ChildVue from './components/Child.vue';
+
+</script>
+<template>
+  <div>    
+<h1>App comp</h1>
+    <input type="text" v-model="inp_val">
+     <button @click="Send">Gonder</button>
+
+
+     <hr>
+
+    
+
+      <ChildVue :arr="arr"  />
+
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      inp_val:"",
+      arr:[],
+      
+
+
+    }
+  },
+  methods:{
+    Send(){
+      this.arr.push(this.inp_val)
+      this.inp_val=""
+    
+    }
+  }
+}
+</script>
---
+++ src/components/Child.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Child comp</h1>
+        <ul>
+      <li v-for="(item,index) in arr" :key="index">{{item}} <button @click="Sil(index)">Delete</button></li>
+     </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+         
+        }
+    },
+    methods:{
+      Sil(index){
+    this.arr.splice(index,1)
+      }
+    },
+    props:["arr"]
+}
+</script>
